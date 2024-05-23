01621788b9bfd6d09b8c87abc2ddc6c701c26ce4
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-05-23 10:38:10 +0300

    1.dag_empty_subpartitions_tables

---
+++ sql/1.dag_empty_subpartitions_tables/EMPTY_SUBPARTITIONS_TABLES_INSERT_RES_CONN.sql
@@ -0,0 +1,6 @@
+INSERT INTO DBADMINDATA.EMPTY_SUBPARTITIONS_TABLES_DB_LIST(DBID, 
+															SUCCESS_CONNECT,
+															ERROR_TEXT)
+															VALUES (:dbid, 
+																	:success_connect,
+																	:error_text)
