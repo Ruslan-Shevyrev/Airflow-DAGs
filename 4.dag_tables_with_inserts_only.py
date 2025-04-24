e569686a11ae6ddefe001814a5210c0565381490
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-04-24 14:04:52 +0300

    add 4.dag_tables_with_inserts_only.py

---
+++ 4.dag_tables_with_inserts_only.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.oracle.hooks.oracle import OracleHook
+from datetime import datetime
+from kafka import KafkaProducer
+
+DEFAULT_ARGS = {"email": ["apexdev@sportmaster.ru"],
+                "email_on_failure": True}
+
+KAFKA_BOOTSTRAP_SERVER = 'oraapex-prod:9092'
+TOPIC = 'TABLES_WITH_INSERTS_ONLY'
+
+dag = DAG(
+    dag_id="tables_with_inserts_only",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="00 01 * * 2",
+    catchup=False,
+    tags=['apex'],
+    default_args=DEFAULT_ARGS
+)
+
+start = EmptyOperator(task_id="start")
+
+
+def get_sql_scripts(connection):
+    sqls = {}
+    with connection.cursor() as cursor:
+        for r in cursor.execute("SELECT s.CODE, s.SCRIPT "
+                                "FROM APP_APEX_MICROSERVICES.V_PYTHON_SQL s "
+                                "WHERE s.PROJECT_NAME = 'tables_with_inserts_only' "
+                                "ORDER BY s.ID "):
+            sqls[str(r[0])] = ''.join(r[1].read())
+    return sqls
+
+
+def _get_info():
+
+    producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
+
+    apex_hook = OracleHook(oracle_conn_id="apex")
+
+    with apex_hook.get_conn() as connection_apex:
+        sql_list = get_sql_scripts(connection_apex)
+        with connection_apex.cursor() as cursor_apex:
+            cursor_apex.execute(sql_list['TABLES_WITH_INSERTS_ONLY_QUEUE_DBS'])
+
+
+get_info = PythonOperator(
+    task_id="get_info",
+    python_callable=_get_info,
+    dag=dag,
+)
+
+start >> get_info
